e5ad4631bb2304a508b47c09ae86e6f7269933e6
Daniil Slobodeniuk <sausernsunny@gmail.com>
2021-10-27 11:30:38 +0500

    New changes

--- tokyo_real_estate/settings.py
+++ tokyo_real_estate/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tokyo-ml.eba-qjriabzm.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['tokyo-ml.eba-qjriabzm.us-west-2.elasticbeanstalk.com',
+     '127.0.0.1']
 
 
 # Application definition
