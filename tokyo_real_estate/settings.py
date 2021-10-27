c15d6a3c94187b443d0231cd0baa7bfa010a5d57
Daniil Slobodeniuk <sausernsunny@gmail.com>
2021-10-27 11:55:46 +0500

    added file

--- tokyo_real_estate/settings.py
+++ tokyo_real_estate/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = 'static'
+# STATIC_ROOT = 'static'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
